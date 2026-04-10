ab01d5e63a0cf343508134608b4afed0254eb4b5
laisiyuan-code <laisiyuan@gmail.com>
2026-04-11 01:29:59 +0800

    Add VEML7700 light sensor functionality

---
+++ Test_Light-Sensor_VEML7700.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+#include <SparkFun_VEML7700_Arduino_Library.h>
+
+#define SDA_PIN 6
+#define SCL_PIN 7
+
+VEML7700 lightSensor;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1500);
+  Wire.begin(SDA_PIN, SCL_PIN);
+
+  if (!lightSensor.begin()) {
+    Serial.println("VEML7700 not detected");
+    while (1) delay(10);
+  }
+
+  Serial.println("VEML7700 OK");
+}
+
+void loop() {
+  Serial.print("Lux: ");
+  Serial.println(lightSensor.getLux(), 2);
+  delay(1000);
+}
